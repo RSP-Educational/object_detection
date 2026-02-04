1766bd9762fadfeda0584c04f2d68ef5fee76049
Robert Schulz <schulzr@gmail.com>
2026-02-04 15:56:08 +0100

    bug fix shape loading points

--- src/data.py
+++ src/data.py
@@ -35,6 +35,7 @@ def load_points_classes(ann_file: str) -> Tuple[List, List]:
     with open(ann_file, 'r') as f:
         annotations = json.load(f)
     points = np.array([[[obj['p1x'], obj['p1y']], [obj['p2x'], obj['p2y']], [obj['p3x'], obj['p3y']]] for obj in annotations])
+    points = points.reshape(-1, 2)
     classes = np.array([obj['class'] for obj in annotations])
     return points, classes
 
